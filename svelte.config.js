beb2068cb2179a10a1bf2318972a9e9bc70831a7
shiki <kishintorii@gmail.com>
2024-12-09 17:14:25 +0900

    :rocket: save

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,26 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	compilerOptions: {
 		runes: false
 	},
 	kit: {
-		outDir: 'dist',
 		adapter: adapter({
-			pages: 'dist',
-			assets: 'dist',
 			fallback: 'index.html',
-			precompress: false
-		})
+			precompress: false,
+			strict: false
+		}),
+		paths: {
+			base: ''
+		},
+		files: {
+			lib: 'src/lib',
+			routes: 'src/routes',
+			assets: 'static'
+		}
 	},
 	onwarn: (warning, handler) => {
 		if (warning.code === 'a11y-click-events-have-key-events') return;
