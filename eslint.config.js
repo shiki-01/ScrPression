97de9e675befd3592d1d19360b56aeec9d579d27
shiki <140712093+shiki-01@users.noreply.github.com>
2024-12-20 01:17:38 +0000

    config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,12 @@
 import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import ts from 'typescript-eslint';
 
 export default ts.config(
 	js.configs.recommended,
 	...ts.configs.recommended,
-	...svelte.configs['flat/recommended'],
 	prettier,
-	...svelte.configs['flat/prettier'],
 	{
 		languageOptions: {
 			globals: {
@@ -19,22 +16,6 @@ export default ts.config(
 		}
 	},
 	{
-		files: ['**/*.svelte'],
-
-		languageOptions: {
-			parserOptions: {
-				parser: ts.parser
-			}
-		}
-	},
-	{
-		rules: {
-			'svelte/valid-compile': ['error', {
-				ignoreWarnings: ['a11y-click-events-have-key-events']
-			}]
-		}
-	},
-	{
-		ignores: ['build/', '.svelte-kit/', 'dist/']
+		ignores: ['build/', 'dist/']
 	}
 );
